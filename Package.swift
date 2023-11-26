3c39da6084c76ff3c06e8eb6328d0cd1645548cc
David Castaneda <davidehcastaneda@gmail.com>
2023-11-25 22:02:23 -0500

    base interceptor setup

--- Package.swift
+++ Package.swift
@@ -5,17 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "LogInterceptor",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LogInterceptor",
             targets: ["LogInterceptor"]),
     ],
+    dependencies: [
+        // Add your package dependencies here
+        .package(url: "https://github.com/davidehcastaneda/NetworkClient", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LogInterceptor"),
+            name: "LogInterceptor",
+            dependencies: ["NetworkClient"]),
         .testTarget(
             name: "LogInterceptorTests",
             dependencies: ["LogInterceptor"]),
--- Sources/LogInterceptor/LogInterceptor.swift
+++ Sources/LogInterceptor/LogInterceptor.swift
@@ -1,2 +1,20 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import NetworkClient
+import Foundation
+
+public class LogInterceptor: Interceptor {
+    public func intercept(request: Request, chain: Chain) async throws -> (Data, URLResponse) {
+        #if DEBUG
+        debugPrint(request)
+        do {
+            let response = try await chain.proceed(request: request)
+            debugPrint(response)
+            return response
+        } catch {
+            debugPrint(error)
+            throw error
+        }
+        #else
+        return try await chain.proceed(request: request)
+        #endif
+    }
+}
