a1d426aa61b92964cfb830687796dcbfc8d54dd5
David Castaneda <davidehcastaneda@gmail.com>
2023-11-25 22:03:17 -0500

    makes init public

--- Sources/LogInterceptor/LogInterceptor.swift
+++ Sources/LogInterceptor/LogInterceptor.swift
@@ -2,6 +2,9 @@ import NetworkClient
 import Foundation
 
 public class LogInterceptor: Interceptor {
+    public init(){
+    }
+
     public func intercept(request: Request, chain: Chain) async throws -> (Data, URLResponse) {
         #if DEBUG
         debugPrint(request)
